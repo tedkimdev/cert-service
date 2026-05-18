ef5d5d7a4fb8877d3ad97b70a9ad584a0c530b30
Ted <dev.ted.kim@gmail.com>
2026-05-18 15:56:47 -0400

    chore: add docker-compose with backend, frontend, and db services

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
 };
 
 export default nextConfig;
